09d6e585433fa8b28c874d1c9fe2a51dfdd1b467
Thuan <mthuan1004@gmail.com>
2023-11-13 13:28:16 +0700

    sochinhphuong

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 using namespace std;
+int ktraSoCP(){
+	int s = 0;
+	for(int i=1; i<n; i++){
+		if((int)sqrt(n) == sqrt(n)){
+			s+= i;
+		}
+	}
+	return s;
+}
 int main(){
-	int n;
-	cout<<"Nhap vao so nguyen n: ";
-	cin >> n;
+	cout <<"So chinh phuong la:" <<ktraSoCP();
 	return 0;
 }
