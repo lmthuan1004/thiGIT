6de7cf1cd8ff86c5a4586fb72199ed2a3afc7127
Thuan <mthuan1004@gmail.com>
2023-11-13 13:25:49 +0700

    sohoanthien

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 using namespace std;
+int ktraSHT(){
+	int s=0;
+	for(int i=1; i<n; i++){
+		if(n%i ==0){
+			s+= i;
+		}
+	}
+	return s==n;
+}
 int main(){
-	int n;
-	cout<<"Nhap vao so nguyen n: ";
-	cin >> n;
-	return 0;
+	cout <<"So hoan thien la:" <<ktraSHT();
 }
